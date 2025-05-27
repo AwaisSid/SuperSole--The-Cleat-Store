8e6433ea1c1c7df714fae899f4c36fe31c39d9fe
AwaisSid <awaiscr7siddiqui@gmail.com>
2025-05-27 18:43:09 +0530

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,510 @@
+
+
+
+
+
+// productslistssssssss
+
+        // 1 PRODUCT NIKE
+const proddiv = document.querySelector(".nike1");
+const close1 = document.querySelector(".close1");
+const buypop = document.querySelector(".div1prod");
+
+proddiv.addEventListener("click",()=>{
+    buypop.style.display="flex";
+});
+
+close1.addEventListener("click",()=>{
+    buypop.style.display="none";
+});
+
+        //2 PRODUCT NIKE 
+const proddiv2 = document.querySelector(".nike2");
+const close2 = document.querySelector(".close2");
+const buypop2 = document.querySelector(".div2prod");
+
+proddiv2.addEventListener("click",()=>{
+    buypop2.style.display="flex";
+})
+
+close2.addEventListener("click",()=>{
+    buypop2.style.display="none";
+})            
+
+        // 3 PRODUCT NIKE
+const proddiv3 = document.querySelector(".nike3");
+const close3 = document.querySelector(".close3");
+const buypop3 = document.querySelector(".div3prod");
+
+proddiv3.addEventListener("click",()=>{
+    buypop3.style.display="flex";
+})
+
+close3.addEventListener("click",()=>{
+    buypop3.style.display="none";
+})            
+
+        // 4 PRODUCT NIKE
+const proddiv4 = document.querySelector(".nike4");
+const close4 = document.querySelector(".close4");
+const buypop4 = document.querySelector(".div4prod");
+
+proddiv4.addEventListener("click",()=>{
+    buypop4.style.display="flex";
+})
+
+close4.addEventListener("click",()=>{
+    buypop4.style.display="none";
+})   
+
+        // 5 PRODUCT NIKE
+const proddiv5 = document.querySelector(".nike5");
+const close5 = document.querySelector(".close5");
+const buypop5 = document.querySelector(".div5prod");
+
+proddiv5.addEventListener("click",()=>{
+    buypop5.style.display="flex";
+})
+
+close5.addEventListener("click",()=>{
+    buypop5.style.display="none";
+})   
+
+        // 6 PRODUCT NIKE
+const proddiv6 = document.querySelector(".nike6");
+const close6 = document.querySelector(".close6");
+const buypop6 = document.querySelector(".div6prod");
+
+proddiv6.addEventListener("click",()=>{
+    buypop6.style.display="flex";
+})
+
+close6.addEventListener("click",()=>{
+    buypop6.style.display="none";
+})  
+
+    // 7 PRODUCT NIKE
+const proddiv7 = document.querySelector(".nike7");
+const close7 = document.querySelector(".close7");
+const buypop7 = document.querySelector(".div7prod");
+
+proddiv7.addEventListener("click",()=>{
+    buypop7.style.display="flex";
+})
+
+close7.addEventListener("click",()=>{
+    buypop7.style.display="none";
+}) 
+
+    // 8 PRODUCT NIKE
+const proddiv8 = document.querySelector(".nike8");
+const close8 = document.querySelector(".close8");
+const buypop8 = document.querySelector(".div8prod");
+
+proddiv8.addEventListener("click",()=>{
+    buypop8.style.display="flex";
+})
+
+close8.addEventListener("click",()=>{
+    buypop8.style.display="none";
+})  
+
+    // 9 PRODUCT NIKE
+const proddiv9 = document.querySelector(".nike9");
+const close9 = document.querySelector(".close9");
+const buypop9 = document.querySelector(".div9prod");
+
+proddiv9.addEventListener("click",()=>{
+    buypop9.style.display="flex";
+})
+
+close9.addEventListener("click",()=>{
+    buypop9.style.display="none";
+})  
+
+    // 10 PRODUCT NIKE
+    const proddiv10 = document.querySelector(".nike10");
+    const close10 = document.querySelector(".close10");
+    const buypop10 = document.querySelector(".div10prod");
+    
+    proddiv10.addEventListener("click",()=>{
+        buypop10.style.display="flex";
+    })
+    
+    close10.addEventListener("click",()=>{
+        buypop10.style.display="none";
+    })  
+
+     // 11 PRODUCT NIKE
+     const proddiv11 = document.querySelector(".adidas1");
+     const close11 = document.querySelector(".close11");
+     const buypop11 = document.querySelector(".div11prod");
+     
+     proddiv11.addEventListener("click",()=>{
+         buypop11.style.display="flex";
+     })
+     
+     close11.addEventListener("click",()=>{
+         buypop11.style.display="none";
+     })  
+    // 12 PRODUCT NIKE
+    const proddiv12 = document.querySelector(".adidas2");
+    const close12 = document.querySelector(".close12");
+    const buypop12 = document.querySelector(".div12prod");
+    
+    proddiv12.addEventListener("click",()=>{
+        buypop12.style.display="flex";
+    })
+    
+    close12.addEventListener("click",()=>{
+        buypop12.style.display="none";
+    })  
+    // 13 PRODUCT NIKE
+    const proddiv13 = document.querySelector(".adidas3");
+    const close13 = document.querySelector(".close13");
+    const buypop13 = document.querySelector(".div13prod");
+    
+    proddiv13.addEventListener("click",()=>{
+        buypop13.style.display="flex";
+    })
+    
+    close13.addEventListener("click",()=>{
+        buypop13.style.display="none";
+    })  
+    // 14 PRODUCT NIKE
+    const proddiv14 = document.querySelector(".adidas4");
+    const close14 = document.querySelector(".close14");
+    const buypop14 = document.querySelector(".div14prod");
+    
+    proddiv14.addEventListener("click",()=>{
+        buypop14.style.display="flex";
+    })
+    
+    close14.addEventListener("click",()=>{
+        buypop14.style.display="none";
+    })  
+    // 15 PRODUCT NIKE
+    const proddiv15 = document.querySelector(".adidas5");
+    const close15 = document.querySelector(".close15");
+    const buypop15 = document.querySelector(".div15prod");
+    
+    proddiv15.addEventListener("click",()=>{
+        buypop15.style.display="flex";
+    })
+    
+    close15.addEventListener("click",()=>{
+        buypop15.style.display="none";
+    })  
+    // 16 PRODUCT NIKE
+    const proddiv16 = document.querySelector(".adidas6");
+    const close16 = document.querySelector(".close16");
+    const buypop16 = document.querySelector(".div16prod");
+    
+    proddiv16.addEventListener("click",()=>{
+        buypop16.style.display="flex";
+    })
+    
+    close16.addEventListener("click",()=>{
+        buypop16.style.display="none";
+    })  
+    // 17 PRODUCT NIKE
+    const proddiv17 = document.querySelector(".adidas7");
+    const close17 = document.querySelector(".close17");
+    const buypop17 = document.querySelector(".div17prod");
+    
+    proddiv17.addEventListener("click",()=>{
+        buypop17.style.display="flex";
+    })
+    
+    close17.addEventListener("click",()=>{
+        buypop17.style.display="none";
+    })  
+    // 18 PRODUCT NIKE
+    const proddiv18 = document.querySelector(".adidas8");
+    const close18 = document.querySelector(".close18");
+    const buypop18 = document.querySelector(".div18prod");
+    
+    proddiv18.addEventListener("click",()=>{
+        buypop18.style.display="flex";
+    })
+    
+    close18.addEventListener("click",()=>{
+        buypop18.style.display="none";
+    })  
+    // 19 PRODUCT NIKE
+    const proddiv19 = document.querySelector(".adidas9");
+    const close19 = document.querySelector(".close19");
+    const buypop19 = document.querySelector(".div19prod");
+    
+    proddiv19.addEventListener("click",()=>{
+        buypop19.style.display="flex";
+    })
+    
+    close19.addEventListener("click",()=>{
+        buypop19.style.display="none";
+    })  
+    // 20 PRODUCT NIKE
+    const proddiv20 = document.querySelector(".adidas10");
+    const close20 = document.querySelector(".close20");
+    const buypop20 = document.querySelector(".div20prod");
+    
+    proddiv20.addEventListener("click",()=>{
+        buypop20.style.display="flex";
+    })
+    
+    close20.addEventListener("click",()=>{
+        buypop20.style.display="none";
+    })  
+    // 21 PRODUCT NIKE
+    const proddiv21 = document.querySelector(".puma1");
+    const close21 = document.querySelector(".close21");
+    const buypop21 = document.querySelector(".div21prod");
+    
+    proddiv21.addEventListener("click",()=>{
+        buypop21.style.display="flex";
+    })
+    
+    close21.addEventListener("click",()=>{
+        buypop21.style.display="none";
+    })  
+    // 22 PRODUCT NIKE
+    const proddiv22 = document.querySelector(".puma2");
+    const close22 = document.querySelector(".close22");
+    const buypop22 = document.querySelector(".div22prod");
+    
+    proddiv22.addEventListener("click",()=>{
+        buypop22.style.display="flex";
+    })
+    
+    close22.addEventListener("click",()=>{
+        buypop22.style.display="none";
+    })  
+    // 23 PRODUCT NIKE
+    const proddiv23 = document.querySelector(".puma3");
+    const close23 = document.querySelector(".close23");
+    const buypop23 = document.querySelector(".div23prod");
+    
+    proddiv23.addEventListener("click",()=>{
+        buypop23.style.display="flex";
+    })
+    
+    close23.addEventListener("click",()=>{
+        buypop23.style.display="none";
+    })  
+    // 24 PRODUCT NIKE
+    const proddiv24 = document.querySelector(".puma4");
+    const close24 = document.querySelector(".close24");
+    const buypop24 = document.querySelector(".div24prod");
+    
+    proddiv24.addEventListener("click",()=>{
+        buypop24.style.display="flex";
+    })
+    
+    close24.addEventListener("click",()=>{
+        buypop24.style.display="none";
+    })  
+    // 25 PRODUCT NIKE
+    const proddiv25 = document.querySelector(".puma5");
+    const close25 = document.querySelector(".close25");
+    const buypop25 = document.querySelector(".div25prod");
+    
+    proddiv25.addEventListener("click",()=>{
+        buypop25.style.display="flex";
+    })
+    
+    close25.addEventListener("click",()=>{
+        buypop25.style.display="none";
+    })  
+    // 26 PRODUCT NIKE
+    const proddiv26 = document.querySelector(".puma6");
+    const close26 = document.querySelector(".close26");
+    const buypop26 = document.querySelector(".div26prod");
+    
+    proddiv26.addEventListener("click",()=>{
+        buypop26.style.display="flex";
+    })
+    
+    close26.addEventListener("click",()=>{
+        buypop26.style.display="none";
+    })  
+    // 27 PRODUCT NIKE
+    const proddiv27 = document.querySelector(".puma7");
+    const close27 = document.querySelector(".close27");
+    const buypop27 = document.querySelector(".div27prod");
+    
+    proddiv27.addEventListener("click",()=>{
+        buypop27.style.display="flex";
+    })
+    
+    close27.addEventListener("click",()=>{
+        buypop27.style.display="none";
+    })  
+    // 28 PRODUCT NIKE
+    const proddiv28 = document.querySelector(".puma8");
+    const close28 = document.querySelector(".close28");
+    const buypop28 = document.querySelector(".div28prod");
+    
+    proddiv28.addEventListener("click",()=>{
+        buypop28.style.display="flex";
+    })
+    
+    close28.addEventListener("click",()=>{
+        buypop28.style.display="none";
+    })  
+    // 29 PRODUCT NIKE
+    const proddiv29 = document.querySelector(".puma9");
+    const close29 = document.querySelector(".close29");
+    const buypop29 = document.querySelector(".div29prod");
+    
+    proddiv29.addEventListener("click",()=>{
+        buypop29.style.display="flex";
+    })
+    
+    close29.addEventListener("click",()=>{
+        buypop29.style.display="none";
+    })  
+    // 30 PRODUCT NIKE
+    const proddiv30 = document.querySelector(".puma10");
+    const close30 = document.querySelector(".close30");
+    const buypop30 = document.querySelector(".div30prod");
+    
+    proddiv30.addEventListener("click",()=>{
+        buypop30.style.display="flex";
+    })
+    
+    close30.addEventListener("click",()=>{
+        buypop30.style.display="none";
+    })  
+    // 31 PRODUCT NIKE
+    const proddiv31 = document.querySelector(".mizuno1");
+    const close31 = document.querySelector(".close31");
+    const buypop31 = document.querySelector(".div31prod");
+    
+    proddiv31.addEventListener("click",()=>{
+        buypop31.style.display="flex";
+    })
+    
+    close31.addEventListener("click",()=>{
+        buypop31.style.display="none";
+    })  
+    // 32 PRODUCT NIKE
+    const proddiv32 = document.querySelector(".mizuno2");
+    const close32 = document.querySelector(".close32");
+    const buypop32 = document.querySelector(".div32prod");
+    
+    proddiv32.addEventListener("click",()=>{
+        buypop32.style.display="flex";
+    })
+    
+    close32.addEventListener("click",()=>{
+        buypop32.style.display="none";
+    })  
+    // 33 PRODUCT NIKE
+    const proddiv33 = document.querySelector(".mizuno3");
+    const close33 = document.querySelector(".close33");
+    const buypop33 = document.querySelector(".div33prod");
+    
+    proddiv33.addEventListener("click",()=>{
+        buypop33.style.display="flex";
+    })
+    
+    close33.addEventListener("click",()=>{
+        buypop33.style.display="none";
+    })  
+    // 34 PRODUCT NIKE
+    const proddiv34 = document.querySelector(".mizuno4");
+    const close34 = document.querySelector(".close34");
+    const buypop34 = document.querySelector(".div34prod");
+    
+    proddiv34.addEventListener("click",()=>{
+        buypop34.style.display="flex";
+    })
+    
+    close34.addEventListener("click",()=>{
+        buypop34.style.display="none";
+    })  
+    // 35 PRODUCT NIKE
+    const proddiv35 = document.querySelector(".mizuno5");
+    const close35 = document.querySelector(".close35");
+    const buypop35 = document.querySelector(".div35prod");
+    
+    proddiv35.addEventListener("click",()=>{
+        buypop35.style.display="flex";
+    })
+    
+    close35.addEventListener("click",()=>{
+        buypop35.style.display="none";
+    })  
+    // 36 PRODUCT NIKE
+    const proddiv36 = document.querySelector(".mizuno6");
+    const close36 = document.querySelector(".close36");
+    const buypop36 = document.querySelector(".div36prod");
+    
+    proddiv36.addEventListener("click",()=>{
+        buypop36.style.display="flex";
+    })
+    
+    close36.addEventListener("click",()=>{
+        buypop36.style.display="none";
+    })  
+    // 37 PRODUCT NIKE
+    const proddiv37 = document.querySelector(".mizuno7");
+    const close37 = document.querySelector(".close37");
+    const buypop37 = document.querySelector(".div37prod");
+    
+    proddiv37.addEventListener("click",()=>{
+        buypop37.style.display="flex";
+    })
+    
+    close37.addEventListener("click",()=>{
+        buypop37.style.display="none";
+    })  
+    // 38 PRODUCT NIKE
+    const proddiv38 = document.querySelector(".mizuno8");
+    const close38 = document.querySelector(".close38");
+    const buypop38 = document.querySelector(".div38prod");
+    
+    proddiv38.addEventListener("click",()=>{
+        buypop38.style.display="flex";
+    })
+    
+    close38.addEventListener("click",()=>{
+        buypop38.style.display="none";
+    })  
+    // 39 PRODUCT NIKE
+    const proddiv39 = document.querySelector(".mizuno9");
+    const close39 = document.querySelector(".close39");
+    const buypop39 = document.querySelector(".div39prod");
+    
+    proddiv39.addEventListener("click",()=>{
+        buypop39.style.display="flex";
+    })
+    
+    close39.addEventListener("click",()=>{
+        buypop39.style.display="none";
+    })  
+    // 40 PRODUCT NIKE
+    const proddiv40 = document.querySelector(".mizuno10");
+    const close40 = document.querySelector(".close40");
+    const buypop40 = document.querySelector(".div40prod");
+    
+    proddiv40.addEventListener("click",()=>{
+        buypop40.style.display="flex";
+    })
+    
+    close40.addEventListener("click",()=>{
+        buypop40.style.display="none";
+    })  
+    // 41 PRODUCT NIKE
+    const proddiv41 = document.querySelector(".mizuno11");
+    const close41 = document.querySelector(".close41");
+    const buypop41 = document.querySelector(".div41prod");
+    
+    proddiv41.addEventListener("click",()=>{
+        buypop41.style.display="flex";
+    })
+    
+    close41.addEventListener("click",()=>{
+        buypop41.style.display="none";
+    })  
+    
